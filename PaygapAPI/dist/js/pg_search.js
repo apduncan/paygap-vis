7bcbeec47c743de6d7df08ecfc95a8fe529b8a30
Unknown <anthonypaulduncan@gmail.com>
2018-05-02 15:26:21 +0100

    Complete

--- PaygapAPI/dist/js/pg_search.js
+++ PaygapAPI/dist/js/pg_search.js
@@ -2,8 +2,6 @@ function co_profile(id) {
     $('#data-container').empty()
     const profile = new CompanyProfile($('#data-container'), id)
     //clear popups
-    $('.highslide-container table').remove()
-    $('.highslide-container .highslide-wrapper').remove()
     return false
 }
 class CompanySearch {
@@ -45,51 +43,6 @@ class CompanySearch {
         })
 
         //add structure for summary bars to be placed in
-        this.elements.bars = $(`<div class="search-bars"></div>`).appendTo(this.elements.container)
-        // this._drawBars()
-    }
-
-    _drawBars() {
-        $(this.elements.bars).empty()
-        var bars = [
-            {class: BandedMeanGapPercentage, value: 0, title: 'Mean Pay Gap (%)', measure: 'co_diff_hourly_mean'},
-            {class: BandedMedianGapPercentage, value: 0, title: 'Median Pay Gap (%)', measure: 'co_diff_hourly_median'},
-            {class: BandedWorkforcePercentage, value: 0, title: 'Workforce Female (%)', measure: 'workforce_female'},
-            {class: BandedQuartileSkew, value: 0, title: 'Quartile Skew', measure: 'quartile_skew', round: 2},
-            {class: BandedMeanBonusGap, value: 0, title: 'Mean Bonus Gap (%)', measure: 'co_diff_bonus_mean'},
-            {class: BandedMedianBonusGap, value: 0, title: 'Median Bonus Gap (%)', measure: 'co_diff_bonus_median'},
-            {class: BandedIndustryDirectorPercentage, value: 0, title: 'Female Directors (%)', measure: 'pc_female'}
-        ]
-        var measureBars = new Array() 
-        for(var i in bars) {
-            const bar = bars[i]
-            const id = `bar${i}`
-            const stack = $(`<div class="profile-vert-bar card interactable-card interactable"></div>`).appendTo(this.elements.bars)
-            const header = $(`<div class="profile-bar-header"></div>`).appendTo(stack)
-            const title = $(`<div>${bar.title}</div>`).appendTo(header)
-            const round = bar.round || 1
-            const value = $(`<div></div>`).appendTo(header)
-            const barDiv = $(`<div id="${id}" class="graph"></div>`).appendTo(stack)
-            const thisBar = new bar.class(`#${id}`, {
-                url: {
-                    sicLevel: 'section',
-                    id: 'Q'
-                },
-            })
-            thisBar.fetchAndDraw()
-            $(`#${id}`).css('order', i)
-            $(`#${id}`).data('natural-order', i)
-            //event handler to display relevant details for each bar
-            const self = this
-            $(stack).click(function() {
-                self._drawDetail(bar)
-            })
-        } 
-        var maxHeader = 0
-        $('.profile-bar-header').each(function(index, element) {
-            if($(element).height() > maxHeader) { maxHeader = $(element).height() }
-        })
-        $('.profile-bar-header').height(maxHeader)
     }
 
     redraw() {
